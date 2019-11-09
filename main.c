65455b4dc3a700b48517aca7d24b150f659bc97b
Tetiana Rohalska <trohalska@e2r12p14.unit.ua>
2019-11-09 10:01:36 +0200

    09.11

--- main.c
+++ main.c
@@ -16,37 +16,9 @@ bool cmp(void *a, void *b) {
 
 int main()
 {	
-	// char *x = (char *)malloc(2 * sizeof(char));
-	// for (int i = 0; i < 2; ++i) {
-	// 	x[i] = '2';
-	// }
-
-	// char **str = (char **)malloc(3 * sizeof(char *));
-	// for (int i = 0; i < 3; ++i) {
-	// 	str[i] = x;
-	// }
-	// free(x);
-
-
-	// for (int i = 0; i < 3; ++i) {
-	// 	str[i] = NULL;
-	// }
-	// free(str);
-	// str = NULL;
-
-	// char x1[] = {"2"};
-
-	// char **test = (char **)malloc(2 * sizeof(char *));
-	// 	for (int i = 0; i < 2; ++i)
-	// 	{
-	// 		test[i] = (char *)malloc(2 * sizeof(char));
-	// 	}
-	// 	mx_del_strarr(&test);
-
 	//TEST CASES!!!
-	//
+
 	//mx_quicksort
-	sleep(1);
 	printf("%s\n", "\x1b[36mTEST FOR mx_quicksort\033[0m");
 	char *sort_test[] = {"Michelangelo", "Donatello", "Leonardo", "Raphael"};
 	if((mx_quicksort(sort_test, 0, 3)) == 2) {
@@ -56,10 +28,11 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: {/Michelangelo/, /Donatello/, /Leonardo/, /Raphael/}, 0, 3\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
+
+	// mx_get_char_index
 	printf("%s\n", "\x1b[36mTEST FOR mx_get_char_index\033[0m");
 	int res1 = mx_get_char_index("123", '2');
-	int res2 = mx_get_char_index("", '7');
+	int res2 = mx_get_char_index(NULL, '7');
 	int res3 = mx_get_char_index("123", '7');
 	if(res1 == 1) {
 		printf("%s\n", "\x1b[32mOK FOR INPUTS: 123, 2\033[0m");
@@ -81,21 +54,19 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: 123, 7\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_strndup//
 	printf("%s\n", "\x1b[36mTEST FOR mx_strndup\033[0m");
 
-	if((mx_strcmp(mx_strndup("122", 2), "123") >= 0)) {
+	if((mx_strcmp(mx_strndup("122", 2), "123") < 0)) {
 		printf("%s\n", "\x1b[32mOK FOR INPUTS: 123, 4\033[0m");
 	}
 	else {
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: 123, 4\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
-	//mx_strndup//
+	
+	// //mx_strndup//
 	printf("%s\n", "\x1b[36mTEST FOR mx_strcpy\033[0m");
 	char src[]="12345226789";
 	char dst[]= "1111111111";
@@ -107,8 +78,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: 12345226789, 1111111111,3\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+	
 	//mx_strcat//
 	printf("%s\n", "\x1b[36mTEST FOR mx_strcat\033[0m");
 	char test_s1[20] = "hello";
@@ -120,8 +90,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: hello, there\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+	
 	//mx_strstr//
 	printf("%s\n", "\x1b[36mTEST FOR mx_strcpy\033[0m");
 	if (mx_strstr("12a34", "2a1") == NULL) {
@@ -137,8 +106,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: 1234, 23\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+	
 	//mx_get_substr_index//
 	printf("%s\n", "\x1b[36mTEST FOR mx_get_substr_index\033[0m");
 	
@@ -173,8 +141,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: null, Don\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+	
 	//mx_count_substr//
 	printf("%s\n", "\x1b[36mTEST FOR mx_count_substr\033[0m");
 	if(mx_count_substr("yo, yo, yo Neo", "yo") == 3) {
@@ -195,15 +162,14 @@ int main()
 	else {
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: NULL, yo\033[0m");
 	}
-	if(mx_count_substr("he  l  o", "lo") == -1) {
-		printf("%s\n", "\x1b[33mFILED FOR INPUTS: \"he  l  o\", lo\033[0m");
+	if(mx_count_substr("he  l  o", "lo") == 0) {
+		printf("%s\n", "\x1b[33mOK FOR INPUTS: \"he  l  o\", lo, if answer is 0\033[0m");
 	}
 	else {
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: \"he  l  o\", lo\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+	
 	//mx_count_words//
 	printf("%s\n", "\x1b[36mTEST FOR mx_count_words\033[0m");
 	if(mx_count_words(" follow * the white rabbit ", '*') == 2) {
@@ -225,8 +191,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: FOR INPUTS: null, / /\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_strtrim//
 	printf("%s\n", "\x1b[36mTEST FOR mx_strtrim\033[0m");
 	if(mx_strlen(mx_strtrim("\t\t\t 1223")) == 4) {
@@ -248,11 +213,10 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: 1223\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_del_extra_spaces
 	printf("%s\n", "\x1b[36mTEST FOR mx_del_extra_spaces\033[0m");
-	if(mx_strlen(mx_del_extra_spaces("\f  My name...   \r  is   Neo  \t\n")) == 18) {
+	if(mx_strlen(mx_del_extra_spaces("\f  My name...   \r  is   Neo  \t\n")) == 17) {
 		printf("%s\n", "\x1b[32mOK FOR INPUTS: \"\\f  My name... is Neo  \\t\\n\"\033[0m");
 	}
 	else {
@@ -265,8 +229,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: NULL\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_strsplit
 	printf("%s\n", "\x1b[36mTEST FOR mx_strsplit\033[0m");
 	char **test = mx_strsplit("**Good bye,**Mr.*Anderson.****", '*');
@@ -298,8 +261,7 @@ int main()
 	}
 	printf("%c", '\n');
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_strjoin
 	printf("%s\n", "\x1b[36mTEST FOR mx_strjoin\033[0m");
 	if(mx_strcmp("dodge this", mx_strjoin("dodge ", "this")) == 0) {
@@ -321,9 +283,8 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: NULL, \"dodge \"\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
-	//mx_file_to_str
+	
+	// mx_file_to_str
 	printf("%s\n", "\x1b[36mTEST FOR mx_file_to_str\033[0m");
 	if(mx_strcmp("12345", mx_file_to_str("test.txt")) == 0) {
 		printf("%s\n", "\x1b[32mOK FOR INPUTS: 12345 in test.txt\033[0m");
@@ -331,9 +292,8 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: 12345 in test.txt\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
-	//mx_file_to_str
+	
+	//mx_replace_substr
 	printf("%s\n", "\x1b[36mTEST FOR mx_replace_substr\033[0m");
 	if(mx_strcmp("McDonuts", mx_replace_substr("McDonalds", "alds", "uts")) == 0) {
 		printf("%s\n", "\x1b[32mOK FOR INPUTS: \"McDonalds\", \"ald\", \"1\"\033[0m");
@@ -354,7 +314,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS: \"McDonalds\", \"alds\", NULL\033[0m");
 	}
 	printf("%c", '\n');
-	//
+
 	//mx_memset
 	printf("%s\n", "\x1b[36mTEST FOR mx_memset\033[0m");
 	char test1[] = "die";
@@ -375,24 +335,27 @@ int main()
 		printf("%s\n", "\x1b[31mFILED OR INPUTS:  \"die\", \"die\", 1\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
-	//mx_memccpy
-	// printf("%s\n", "\x1b[36mTEST FOR mx_memccpy\033[0m");
-	// char dst_test4[] = "  ";
-	// char src_test4[] = "123 456";
 
-	// printf("%s", mx_memccpy(dst_test4, src_test4, 52, 2));
-	
-	// if(mx_strcmp("12", mx_memccpy(dst_test4, src_test4, 52, 2)) == 0) {
-	// 	printf("%s\n", "\x1b[32mOK FOR INPUTS:  \"123 456\", \"  \", 52, 2\033[0m");
-	// }
-	// else {
-	// 	printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"123 456\", \"  \", 52, 2\033[0m");
-	// }
-	// printf("%c", '\n');
-	// sleep(1);
-	//
+	//mx_memccpy
+	printf("%s\n", "\x1b[36mTEST FOR mx_memccpy\033[0m");
+	char d[] = "98765432198";
+    char s[] = "0123456789";	
+	if(mx_memccpy(d, s, 's', 7) == NULL) {
+		printf("%s\n", "\x1b[32mtest1 OK\033[0m");
+	}
+	else {
+		printf("%s\n", "\x1b[31mFILED test1\033[0m");
+	}
+	char u[] = "98765432198";
+    char v[] = "0123456789";
+	if(mx_strcmp("32198", mx_memccpy(u, v, '5', 7)) == 0) {
+		printf("%s\n", "\x1b[32mtest2 OK\033[0m");
+	}
+	else {
+		printf("%s\n", "\x1b[31mFILED test2\033[0m");
+	}
+	printf("%c", '\n');
+
 	//mx_memcmp
 	printf("%s\n", "\x1b[36mTEST FOR mx_memcmp\033[0m");
 	if(mx_memcmp("1234", "1a34", 2) != 0) {
@@ -408,8 +371,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"1234\", \"1234\", 200\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_memchr
 	printf("%s\n", "\x1b[36mTEST FOR mx_memchr\033[0m");
 	if(mx_strcmp("234", mx_memchr("1234", 50, 4)) == 0) {
@@ -419,8 +381,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"1234\", 50, 4\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_memrchr
 	printf("%s\n", "\x1b[36mTEST FOR mx_memrchr\033[0m");
 	if(mx_strcmp("ity", mx_memrchr("Trinity", 'i', 7)) == 0) {
@@ -430,8 +391,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"Trinity\", \'i\', 7\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_memmem
 	printf("%s\n", "\x1b[36mTEST FOR mx_memmem\033[0m");
 	if(mx_strcmp("123taa", (mx_memmem("aaa123taa", 7, "123", 3))) == 0) {
@@ -441,8 +401,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"aaa123taa\", 7,  \"123\", 3\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_memmove
 	printf("%s\n", "\x1b[36mTEST FOR mx_memmove\033[0m");
 	char test_dst1 []= "1234567890";
@@ -454,8 +413,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"1234567890\", \"567890\",  \"4567890\", 3\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
-	//
+
 	//mx_realoc
 	printf("%s\n", "\x1b[36mTEST FOR mx_realoc\033[0m");
 	char *test_relloc = (char *) malloc(10 * sizeof(char));
@@ -474,7 +432,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"aaaaaaaaaa\", 120\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
+	
 	printf("%s\n", "\x1b[36mTEST FOR mx_create_node\033[0m");
 	
 	t_list *test_tlist1 = mx_create_node("123");
@@ -485,7 +443,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  \"123\"\033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
+	
 	t_list *node1 = mx_create_node("2");
 	t_list *node2 = mx_create_node("1");
 	node1->next = node2;
@@ -506,7 +464,7 @@ int main()
 		printf("%s\n", "\x1b[31mFILED FOR INPUTS:  two nodes \033[0m");
 	}
 	printf("%c", '\n');
-	sleep(1);
+	
 	printf("%s\n", "\x1b[36mTEST FOR mx_sort_list\033[0m");
 	printf("%s\n", "INPUTS: 2,1");
 	printf("%s", "OUTPUTS: ");
